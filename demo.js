842ccc5200ec1f37d1e778afce20e46f82f9a06f
TrungDuong1207 <119021139+TrungDuong1207@users.noreply.github.com>
2023-03-19 13:11:06 +0700

    delete demo.js

--- demo.js
+++
@@ -1,9 +0,0 @@
-var mergeTwoLists = function(list1, list2) {
-    let arr = [...list1, ...list2].sort((a,b)=>{
-        return a-b;
-    });
-    return arr
-};
-let list1 = [1,2,4];
-let list2 = [1,3,4];
-console.log(mergeTwoLists(list1, list2));
